2ed504b0bce420aac72eaffe46657f567bdc09ee
ZHAO Jing <ceo@webturing.com>
2019-08-04 11:30:59 +0800

    Add files via upload

---
+++ 20190804AM/区间素数统计.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+inline bool prime(int n){
+	if(n==2)return true;
+	if(n<=1 || n%2==0)return false;
+	int m=sqrt(n);
+	for(int i=3;i<=m;i+=2){
+		if(n%i==0)
+		return false;
+	}
+	return true;
+}
+int main() {
+	int a,b;
+	cin>>a>>b;
+	int tot=0;
+	if(a<=2 and 2<=b)++tot;
+	if(a%2==0)++a;
+	
+	for(int n=a;n<=b;n+=2){
+		if(prime(n))
+		++tot;
+		
+	} 
+	cout<<tot<<endl;
+	return 0;
+}
---
+++ 20190804AM/扫雷.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	freopen("input.txt","r",stdin);
+	int n,m,k;
+	cin>>n>>m>>k ;
+	int G[n][m];
+	//cerr<<"Size of G="<<sizeof(G)<<endl;
+	memset(G[0],0,sizeof(G));
+	while(k--) {
+		int x,y;
+		cin>>x>>y;
+		G[x][y]=9;
+	}
+
+	for(int i=0;i<n;i++)
+	for(int j=0;j<m;j++){
+		if(G[i][j]==9)continue;
+		int s=0;
+		for(int dx=-1;dx<=1;dx++)
+		for(int dy=-1;dy<=1;dy++){
+			if(dx==0&&dy==0)continue;
+			int nx=dx+i;
+			int ny=dy+j;
+			if(nx<0||nx>=n||ny<0||ny>=m) continue;
+			if(G[nx][ny]==9)++s;
+		}
+		G[i][j]=s;
+		
+	}
+		for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++){
+			
+			cerr<<G[i][j];
+		}
+		cerr<<endl;
+	}
+	return 0;
+}
