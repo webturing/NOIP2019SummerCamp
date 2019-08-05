a0c40a5f99129b4416fcb052558781fd6ca0cbda
webturing <zj@webturing.com>
2019-08-05 13:43:55 +0800

    math

--- 20190805AM/solutions/I.cpp
+++ 20190805AM/solutions/I.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int lcm(int a, int b) { return a * b / __gcd(a, b); }
 
 int main() {
-    int n, a, b;oj
+    int n, a, b;
     for (cin >> n; n-- && cin >> a >> b;
          cout << __gcd(a, b) << " " << lcm(a, b) << endl);
     return 0;
